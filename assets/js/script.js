ee909632eef27cd6796f628107dc965d02b7dcba
JosefinLarsson <sarajosefinlarsson@gmail.com>
2022-12-12 16:07:39 +0000

    Add js initial functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,25 @@
 /**
- * Declare variables for DOM elements
+ * Declare const for DOM elements
  * and possible choices
  */
 
-let buttons = document.getElementsByClassName("buttons");
-let userScore = document.getElementById("user-score");
-let computerScore = document.getElementById("computer-score");
-let userImage = document.getElementById("user-image");
-let computerImage = document.getElementById("computer-image");
-let resultAlert = document.getElementById("result-alert");
-let choices = ["rock", "paper", "scissors"];
+const buttons = document.getElementsByClassName("button");
+const userScore = document.getElementById("user-score");
+const computerScore = document.getElementById("computer-score");
+const userImage = document.getElementById("user-image");
+const computerImage = document.getElementById("computer-image");
+const resultAlert = document.getElementById("result-alert");
+const choices = ["rock", "paper", "scissors"];
 
 /**
  * Add event listener to buttons
  */
 
 for (let button of buttons) {
-    button.addEventListener("click", function()) {
+    button.addEventListener("click", function(playGame)) {
         let userChoice = this.getAttribute("data-choice");
         playGame(userChoice);
-    });
+    };
 }
 
 /**
@@ -31,8 +31,34 @@ function playGame(userChoice) {
     userImage.src = `assets/images/${choices[userChoice]}.png`;
     userImage.alt = choices[userChoice];
 
-    let computerChoice = Math.floor(Math.random() * 3);
+    let computerChoice = Math.floor(Math.random("data-choice") * 3);
 
     computerImage.src = `assets/images/${choices[computerChoice]}.png`;
     computerImage.alt = choices[computerChoice];
+
+    let result = checkWinner(choices[computerChoice], choices[userChoice]);
+
+    updateScores(result);
 }
+
+
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+
+
+/**function incrementUserScore() {
+
+    let oldScore = parseInt(document.getElementById("user-score").innerText);
+    document.getElementById("user-score").innerText = ++oldScore;
+
+}
+
+function incrementComputerScore() {
+
+    let oldScore = parseInt(document.getElementById("computer-score").innerText);
+    document.getElementById("computer-score").innerText = ++oldScore;
+
+}
+*/
+
